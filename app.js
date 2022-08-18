1c760fdc0765aab8557a1f32019810cf7d80c801
Julien Mihai <mihaijulien@gmail.com>
2022-08-18 20:52:47 +0300

    ejs basci setup

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
 app.listen(PORT, function(){
     console.log('listening on port: ' + PORT);
-})
+});

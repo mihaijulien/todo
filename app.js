4422930ca5644b64b21f5975286ef966cb71477b
Julien Mihai <mihaijulien@gmail.com>
2022-08-19 14:49:14 +0300

    mongodb setup

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+const dburl = "mongodb://localhost:27017/tododb";
+mongoose.connect(dburl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', function(req, res){
     res.render('home');
